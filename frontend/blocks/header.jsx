0fa112f63f61fdfb2dff099f786cdb842b61d464
den59k <den59k@gmail.com>
2022-12-13 19:42:20 +0500

    feat: change title

--- frontend/blocks/header.jsx
+++ frontend/blocks/header.jsx
@@ -8,7 +8,7 @@ export default function Header (props){
     <div className={styles.background} style={{backgroundImage: `url(/photo.jpg)`}}>
       <div className={styles.overlay}>
         <h2>Ремонт офисов и квартир</h2>
-        <h1>Polytech</h1>
+        <h1>EgorMaster</h1>
         <div className={styles.sub}>Уже на протяжении 5 лет делаем<br/>качественный современный ремонт</div>
         <a href="/#form" className={cn("btn", styles.button)}>Заказать ремонт</a>
       </div>
